8717e22d4dc63e0fef7aa8e1937e12da65fa5993
AlexPearsall <pearsalla@msoe.edu>
2024-12-05 16:04:53 -0600

    Moved CenterNode to ProjectNodes.jsx

--- web/src/components/Grid.jsx
+++ web/src/components/Grid.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import CenterNode from './CenterNode';
-
-const defXPos = 338;
-const defYPos = 610;
-const centerPointX = defXPos + 16
-const centerPointY = defYPos + 16
 
 function Grid() {
-
-    const cn = <CenterNode topSize={defXPos} leftSize={defYPos}/>
 
     return (
         <div className="grid-container">
             {[...Array(289)].map((_, index) =>
                 <div className="grid-item" key={index}></div>
             )}
-            <div className={"project-nodes"}>
-                {cn}
-            </div>
         </div>
     );
 }
